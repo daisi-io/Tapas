c0ba9847ade81a08da9cbdc8281110f91b905935
zhenshan <zhenshan@belmont.tech>
2022-02-18 21:33:57 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from predict import unzip_model, load_model_and_tokenizer, prepare_inputs, generate_predictions, postprocess_predictions, show_answers
+
+# get model_zip_path
+model_zip_path = "/Users/zhenshanjin/Documents/Belmont/sandy/UtilityDaisies/tapas-base-finetuned-wtq/tmp/tapas-base-finetuned-wtq.zip"
+model_path = unzip_model(model_zip_path)
+tokenizer, model = load_model_and_tokenizer(model_path)
+
+
+def run_tapas(table_json_path:pd.DataFrame, query):
+    """
+    Invoke the TAPAS model.
+    """
+    queries = [query]
+    table, inputs = prepare_inputs(table_json_path, queries, tokenizer)
+    predicted_table_cell_coords, predicted_aggregation_operators = generate_predictions(inputs, model, tokenizer)
+    aggregation_predictions_string, answers = postprocess_predictions(predicted_aggregation_operators, predicted_table_cell_coords, table)
+    answers = show_answers(queries, answers, aggregation_predictions_string)
+
+    return {"answers": answers}
+
+
+if __name__ == '__main__':
+    # Define the table
+    data = {'Cities': ["Paris, Franc    e", "London, England", "Lyon, France"], 'Inhabitants': ["2.161", "8.982", "0.513"]}
+
+    # Define the questions
+    # queries = ["Which city has most inhabitants?", "What is the average number of inhabitants?", "How many French cities are in the list?", "How many inhabitants live in French cities?"]
+    query = "Which city has most inhabitants?"
+    res = run_tapas(data, query)
---
+++ predict.py
@@ -0,0 +1,106 @@
+import os
+import pathlib
+from transformers import TapasTokenizer, TapasForQuestionAnswering
+import pandas as pd
+import zipfile
+import json
+
+
+def unzip_model(zip_model_path):
+    p = pathlib.Path(zip_model_path)
+    root = str(p.parents[0])
+    print(f'start unzip: {root}')
+    model_path = os.path.join(root, os.path.splitext(os.path.basename(zip_model_path))[0] + '/')
+    if not os.path.isdir(model_path):
+        with zipfile.ZipFile(zip_model_path, "r") as zip_ref:
+            zip_ref.extractall(root)
+        
+    print(f"finish zipping model to {model_path}")
+    return model_path
+
+
+def load_model_and_tokenizer(model_path):
+    """
+    Load
+    """
+    # Load pretrained tokenizer: TAPAS finetuned on WikiTable Questions
+    tokenizer = TapasTokenizer.from_pretrained(model_path)
+
+    # Load pretrained model: TAPAS finetuned on WikiTable Questions
+    model = TapasForQuestionAnswering.from_pretrained(model_path)
+
+    # Return tokenizer and model
+    return tokenizer, model
+
+
+def prepare_inputs(table_json_path, queries, tokenizer):
+    """
+    Convert dictionary into data frame and tokenize inputs given queries.
+    """
+    with open(table_json_path, "r") as istr:
+        data = json.load(istr)
+    # Prepare inputs
+    table = pd.DataFrame.from_dict(data)
+    inputs = tokenizer(table=table, queries=queries, padding='max_length', return_tensors="pt")
+
+    # Return things
+    return table, inputs
+
+
+def generate_predictions(inputs, model, tokenizer):
+    """
+    Generate predictions for some tokenized input.
+    """
+    # Generate model results
+    outputs = model(**inputs)
+
+    # Convert logit outputs into predictions for table cells and aggregation operators
+    predicted_table_cell_coords, predicted_aggregation_operators = tokenizer.convert_logits_to_predictions(
+            inputs,
+            outputs.logits.detach(),
+            outputs.logits_aggregation.detach()
+    )
+
+    # Return values
+    return predicted_table_cell_coords, predicted_aggregation_operators
+
+
+def postprocess_predictions(predicted_aggregation_operators, predicted_table_cell_coords, table):
+    """
+    Compute the predicted operation and nicely structure the answers.
+    """
+    # Process predicted aggregation operators
+    aggregation_operators = {0: "NONE", 1: "SUM", 2: "AVERAGE", 3:"COUNT"}
+    aggregation_predictions_string = [aggregation_operators[x] for x in predicted_aggregation_operators]
+
+    # Process predicted table cell coordinates
+    answers = []
+    for coordinates in predicted_table_cell_coords:
+        if len(coordinates) == 1:
+            # 1 cell
+            answers.append(table.iat[coordinates[0]])
+        else:
+            # > 1 cell
+            cell_values = []
+            for coordinate in coordinates:
+                cell_values.append(table.iat[coordinate])
+                answers.append(", ".join(cell_values))
+        
+    # Return values
+    return aggregation_predictions_string, answers
+
+
+def show_answers(queries, answers, aggregation_predictions_string):
+    """
+    Visualize the postprocessed answers.
+    """
+    result = []
+    for query, answer, predicted_agg in zip(queries, answers, aggregation_predictions_string):
+        res = {"query": query}
+        if predicted_agg == "NONE":
+            res["answer"] = answer
+        else:
+            res["answer"] = predicted_agg + " > " + answer
+        result.append(res)
+
+    return result
