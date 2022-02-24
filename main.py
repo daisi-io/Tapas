28fb823fb7753e186c598c26c86aadd56e6b2586
zhenshan <zhenshan@belmont.tech>
2022-02-24 09:00:16 -0600

    add unzip routine

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import pandas as pd
 from predict import unzip_model, load_model_and_tokenizer, prepare_inputs, generate_predictions, postprocess_predictions, show_answers
 
 # get model_zip_path
-# model_path = unzip_model(model_zip_path)
-model_path = "/pebble_tmp/tmp/model_tapas"
+model_zip_path = "/pebble_tmp/tmp/tapas-base-finetuned-wtq.zip"
+model_path = unzip_model(model_zip_path)
 tokenizer, model = load_model_and_tokenizer(model_path)
 
 
@@ -11,6 +11,7 @@ def run_tapas(table_json_path:pd.DataFrame, query):
     """
     Invoke the TAPAS model.
     """
+    print(f"model_path: ", model_path)
     queries = [query]
     table, inputs = prepare_inputs(table_json_path, queries, tokenizer)
     predicted_table_cell_coords, predicted_aggregation_operators = generate_predictions(inputs, model, tokenizer)
